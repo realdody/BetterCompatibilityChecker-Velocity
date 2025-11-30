62aefacb151f5b13c14990c485decbbee173969c
realdody <65219829+realdody@users.noreply.github.com>
2025-11-30 09:47:05 +0100

    Potential Velocity fix 1.21.1

--- src/main/java/dev/wuffs/bcc/mixin/ClientboundStatusResponsePacketMixin.java
+++ src/main/java/dev/wuffs/bcc/mixin/ClientboundStatusResponsePacketMixin.java
@@ -11,7 +11,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import com.google.gson.JsonElement;
+import net.minecraft.Util;
+import com.mojang.serialization.JsonOps;
+import net.minecraft.util.GsonHelper;
+import com.mojang.serialization.Codec;
 
 @Mixin(value = ClientboundStatusResponsePacket.class)
 public class ClientboundStatusResponsePacketMixin {
@@ -19,19 +25,27 @@ public class ClientboundStatusResponsePacketMixin {
     @Final
     private ServerStatus status;
 
-    @Inject(method = "write", at = @At("TAIL"))
-    void onWrite(FriendlyByteBuf p_134899_, CallbackInfo ci) {
-        p_134899_.writeJsonWithCodec(BetterStatus.CODEC, BetterStatusServerHolder.INSTANCE.getStatus());
+    @Redirect(method = "write", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/FriendlyByteBuf;writeJsonWithCodec(Lcom/mojang/serialization/Codec;Ljava/lang/Object;)V"))
+    private void onWrite(FriendlyByteBuf instance, Codec<ServerStatus> codec, Object status) {
+        BetterStatus betterStatus = BetterStatusServerHolder.INSTANCE.getStatus();
+        JsonElement json = codec.encodeStart(JsonOps.INSTANCE, (ServerStatus) status).result().orElseThrow(() -> new IllegalStateException("Failed to encode status"));
+        if (betterStatus != null) {
+            JsonElement betterStatusJson = BetterStatus.CODEC.encodeStart(JsonOps.INSTANCE, betterStatus).result().orElseThrow(() -> new IllegalStateException("Failed to encode better status"));
+            json.getAsJsonObject().add("bcc", betterStatusJson);
+        }
+        instance.writeUtf(GsonHelper.toStableString(json));
     }
 
-    @Inject(method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V", at = @At("TAIL"))
-    void onReadConstructor(FriendlyByteBuf buffer, CallbackInfo ci) {
-        // Ensure the byte reader doesn't go out of range as non-modded servers / servers without our mod will not be able
-        // to read the json bytes that do not exist
-        if (buffer.readerIndex() + 1 > buffer.capacity()) {
-            return;
+    @Redirect(method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/FriendlyByteBuf;readJsonWithCodec(Lcom/mojang/serialization/Codec;)Ljava/lang/Object;"))
+    private Object onRead(FriendlyByteBuf instance, Codec<ServerStatus> codec) {
+        JsonElement json = GsonHelper.parse(instance.readUtf());
+        ServerStatus status = codec.parse(JsonOps.INSTANCE, json).result().orElseThrow(() -> new IllegalStateException("Failed to parse status"));
+
+        if (json.isJsonObject() && json.getAsJsonObject().has("bcc")) {
+            BetterStatus betterStatus = BetterStatus.CODEC.parse(JsonOps.INSTANCE, json.getAsJsonObject().get("bcc")).result().orElseThrow(() -> new IllegalStateException("Failed to parse better status"));
+            ((ServerDataExtension) (Object) status).setBetterData(betterStatus);
         }
 
-        ((ServerDataExtension) (Object) status).setBetterData(buffer.readJsonWithCodec(BetterStatus.CODEC));
+        return status;
     }
 }
